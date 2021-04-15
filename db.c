001efb989d47d89495953c29953499bad2774ca1
vts <vithurshan@laposte.net>
2021-04-15 22:25:03 +0200

    add some fonction

--- db.c
+++ db.c
@@ -31,6 +31,7 @@ typedef struct {
   Row row_to_insert;
 } Statement;
 
+// oder in which to save row data
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
 const uint32_t ID_SIZE = size_of_attribute(Row, id); // 4 bytes
@@ -40,6 +41,20 @@ const uint32_t ID_OFFSET = 0;
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE; // 4 bytes
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; // 4 + 32 = 36 bytes
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE; // 4 + 32 + 255 = 291 bytes
+
+// stop at this point - still not understand why & infront of pointer
+void serialize_row(Row* source, void* destination) {
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row* destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
