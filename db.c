a4d0089ce82981ebe08ec760eed19caa429fa552
vts <vithurshan@laposte.net>
2021-04-15 16:24:53 +0200

    Move debug files to test folder

--- db.c
+++ db.c
@@ -15,13 +15,31 @@ typedef enum {
 } MetaCommandResult;
 
 typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
-
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 
 typedef struct {
   StatementType type;
+  Row row_to_insert;
 } Statement;
 
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id); // 4 bytes
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username); // 32 bytes
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email); //255 bytes
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE; // 4 bytes
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; // 4 + 32 = 36 bytes
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE; // 4 + 32 + 255 = 291 bytes
 
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
@@ -67,10 +85,18 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
   }
 }
 
+// check prepare statement
 PrepareResult prepare_statement(InputBuffer* input_buffer,
                                 Statement* statement) {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+      input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+      statement->row_to_insert.username, statement->row_to_insert.email);
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    }
+
     return PREPARE_SUCCESS;
   }
   if (strcmp(input_buffer->buffer, "select") == 0) {
---
+++ test/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+int main() {
+
+  return 0;
+}
