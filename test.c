e3818ae3222a4fd3a89a2463f74851e95f7cc3a6
vts <vithurshan@laposte.net>
2021-04-22 15:49:39 +0200

    use of "break;" to behaviour

--- test.c
+++ test.c
@@ -2,12 +2,29 @@
 #include <stdint.h>
 
 int main(int argc, char const *argv[]) {
-  int user_age[2];
-  int *age = user_age;
-  printf("%d\n", sizeof(age));
+  // int user_age[2];
+  // int *age = user_age;
+  // printf("%d\n", sizeof(age));
+  //
+  // age++;
+  // printf("%d\n", sizeof(age));
 
-  age++;
-  printf("%d\n", sizeof(age));
+  int status =1;
+  int age = 0;
 
+  while (1) {
+    scanf("%d", &status);
+
+    switch (status) {
+      case (1):
+      break;
+      case (0):
+      printf("please enter 0 to exit \n");
+      continue;
+    }
+
+  }
+
+  printf("i am after switch statement\n");
   return 0;
 }
