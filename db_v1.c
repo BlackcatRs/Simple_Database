d8b8881cdf3bde7ff3ac9c27943f84eac5f47fb0
BlackcatRs <vithurshan@laposte.net>
2021-05-01 10:17:48 +0200

    trying to understand use of all struct

--- db_v1.c
+++ db_v1.c
@@ -98,6 +98,41 @@ void deserialize_row(void *source, Row* destination) {
   memcpy(&(destination->email), source  EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+// get specific page from file
+void* get_page(Pager* pager, uint32_t page_num) {
+  if (page_num > TABLE_MAX_PAGES) {
+    printf("Tried to fetch page number out of bounds. %d > %d\n", page_num,
+           TABLE_MAX_PAGES);
+    exit(EXIT_FAILURE);
+  }
+
+  if (pager->pages[page_num] == NULL) {
+    // Cache miss. Allocate memory and load from file.
+    void* page = malloc(PAGE_SIZE);
+
+    // total nb of pages in a file
+    uint32_t num_pages = pager->file_length / PAGE_SIZE;
+    // We might save a partial page at the end of the file
+    if (pager->file_length % PAGE_SIZE) {
+      num_pages += 1;
+    }
+
+    if (page_num <= num_pages) {
+      lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+      ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+      if (bytes_read == -1) {
+        printf("Error reading file: %d\n", errno);
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    // loads 4096 bytes of data to mem
+    pager->pages[page_num] = page;
+  }
+
+  return pager->pages[page_num];
+}
+
 // indicate where to write row in a page
 void* row_slot(Table* table, uint32_t row_num) {
   uint32_t page_num = row_num / ROWS_PER_PAGE;
@@ -111,6 +146,7 @@ void* row_slot(Table* table, uint32_t row_num) {
   return page  byte_offset;
 }
 
+// initialize table
 Table* db_open(const char* filename) {
   Pager* pager = pager_open(filename);
   uint32_t num_rows = pager->file_length / ROW_SIZE; //nb of rows in a page
@@ -172,6 +208,49 @@ InputBuffer* new_input_buffer() {
 void close_input_buffer(InputBuffer* input_buffer) {
   free(input_buffer);
 }
+
+// stop here trying to understand use of all struct
+void db_close(Table* table) {
+  Pager* pager = table->pager;
+  uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+  for (uint32_t i = 0; i < num_full_pages; i++) {
+    if (pager->pages[i] == NULL) {
+      continue;
+    }
+    pager_flush(pager, i, PAGE_SIZE);
+    free(pager->pages[i]);
+    pager->pages[i] = NULL;
+  }
+
+  // There may be a partial page to write to the end of the file
+  // This should not be needed after we switch to a B-tree
+  uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+  if (num_additional_rows > 0) {
+    uint32_t page_num = num_full_pages;
+    if (pager->pages[page_num] != NULL) {
+      pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+      free(pager->pages[page_num]);
+      pager->pages[page_num] = NULL;
+    }
+  }
+
+  int result = close(pager->file_descriptor);
+  if (result == -1) {
+    printf("Error closing db file.\n");
+    exit(EXIT_FAILURE);
+  }
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    void* page = pager->pages[i];
+    if (page) {
+      free(page);
+      pager->pages[i] = NULL;
+    }
+  }
+  free(pager);
+  free(table);
+}
+
 
 // .exit
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
