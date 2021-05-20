fde25a2286d71fb311f366235fe9633c184554a5
BlackcatRs <vithurshan@laposte.net>
2021-05-20 21:54:08 +0200

    end of Part 8 - B-Tree Leaf Node Format

--- db_v1.c
+++ db_v1.c
@@ -153,6 +153,25 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
   return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
+void print_constants() {
+  printf("ROW_SIZE: %d\n", ROW_SIZE);
+  printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+  printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+  printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+  printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void print_leaf_node(void* node) {
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  printf("leaf (size %d)\n", num_cells);
+  for (uint32_t i = 0; i < num_cells; i++) {
+    uint32_t key = *leaf_node_key(node, i);
+    printf("  - %d : %d\n", i, key);
+  }
+}
+
+
 // field num_cells = 0
 void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
 
@@ -382,6 +401,14 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
     close_input_buffer(input_buffer);
     db_close(table);
     exit(EXIT_SUCCESS);
+  } else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    printf("Tree:\n");
+    print_leaf_node(get_page(table->pager, 0));
+    return META_COMMAND_SUCCESS;
+  } else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+    printf("Constants:\n");
+    print_constants();
+    return META_COMMAND_SUCCESS;
   } else {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
   }
@@ -447,7 +474,7 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
 
   if (cursor->cell_num < num_cells) {
     // Make room for new cell
-    // tring to understand this fonction
+    // the last cell is blank so we can move a cell
     for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
       memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1),
              LEAF_NODE_CELL_SIZE);
@@ -461,7 +488,8 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
 
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
-  if (table->num_rows >= TABLE_MAX_ROWS) {
+  void* node = get_page(table->pager, table->root_page_num);
+  if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
      return EXECUTE_TABLE_FULL;
   }
 
@@ -470,8 +498,7 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 
   Cursor* cursor = table_end(table);
 
-  serialize_row(row_to_insert, cursor_value(cursor));
-  table->num_rows += 1;
+  leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 
   free(cursor);
 
