f8ac85f197b1ce0f485044eb74b5e4f5e96c9df8
vts <vithurshan@laposte.net>
2021-05-27 16:51:33 +0200

    program test compile db

---
+++ gen_data_db.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+for value in range(1, 15):
+    print("insert " + str(value) + " user" + str(value) + " user" + str(value) + "@codinglab.fr")
+
+
+# insert 15 user15 user15codinglab.fr
